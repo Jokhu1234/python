7c3f601314119c0dd45947a133a8969dbf8192c3
SuzanneJokhu <[sjokhu25@gmail.com]>
2024-07-23 12:55:58 -0400

    saving

---
+++ code-0724/day1/hello.py
@@ -0,0 +1,5 @@
+print(2 + 4)
+message = "hello-world"
+print(message)
+
+print("Welcome to CBC!")
---
+++ code-0724/day2/variables.py
@@ -0,0 +1,19 @@
+"""
+a = 10 
+b = "Salah"
+print(a)
+print(b)
+
+"""
+
+# print(type(a))
+# print(type(b))
+
+# Casting
+
+x = 1045
+print(x)
+
+x = str(x)
+print(type(x))
+
